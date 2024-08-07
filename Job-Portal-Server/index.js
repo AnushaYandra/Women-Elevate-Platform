53e01144ba9ab98ecc07b7d33883d1fe62087325
Anusha Yandra <141119963+AnushaYandra@users.noreply.github.com>
2024-08-07 06:49:52 +0530

    Update index.js

--- Job-Portal-Server/index.js
+++ Job-Portal-Server/index.js
@@ -7,8 +7,13 @@ const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json())
-app.use(cors());
-
+app.use(cors(
+    {
+        origin : ["https://women-elevate-platform.vercel.app/"],
+        methods : ["GET", "POST", "DELETE", "PATCH"],
+        credentials : true
+    }
+));
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGODB_URI;
